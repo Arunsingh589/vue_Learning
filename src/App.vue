e93ba07968f5d8f9ceb9959fc3301aadada05d91
Arun Singh <as3945787@gmail.com>
2025-01-22 11:53:54 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+// Options API
+// export default {
+//   data() {
+//     return {
+//     count: 0
+//     }
+//   },
+//   methods: {
+//     increment(){
+//       this.count++
+//     }
+//   },
+//   mounted(){
+//     console.log(`The initial count is ${this.count}`);
+    
+//   }
+
+// }
+
+// Composition API
+const count = ref(0)
+function increment() {
+  count.value++;
+}
+
+onMounted(() => {
+  console.log(`The initial value is ${count.value}`);
+
+}) 
+  
+
+
+</script>
+
+<template>
+    <button @click="increment">
+      Count: {{ count }}
+
+    </button>
+
+</template>
+
+<style scoped>
+p{
+  color: black;
+}
+</style>
+
+
